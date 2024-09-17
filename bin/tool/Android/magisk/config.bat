8a8202ac1a39794a53c54b3b7e0dc05cb5ab440e
MZY <153284258+ggboyndj@users.noreply.github.com>
2024-09-17 17:05:28 +0800

    Add files via upload

---
+++ bin/tool/Android/magisk/config.bat
@@ -0,0 +1,10 @@
+::此脚本中可设置Magisk修补配置. 若设置, 修补时将以此文件为准.
+::注: arch可选值为   arm_64   arm_32   x86_64   x86_32
+
+::以下是默认配置：
+::set KEEPVERITY=true
+::set KEEPFORCEENCRYPT=true
+::set PATCHVBMETAFLAG=false
+::set RECOVERYMODE=false
+::set LEGACYSAR=true
+::set arch=arm_64
